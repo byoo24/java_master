8515ccd86ed7126fc3a03c1b6c3c4a195efe2e0e
Brian Yoo <byoo.dev@gmail.com>
2021-03-18 21:51:51 -0400

    added ByteShortIntLong

---
+++ ByteShortIntLong/src/byteshortintlong/ByteShortIntLong.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byteshortintlong;
+
+/**
+ *
+ * @author brianyoo
+ */
+public class ByteShortIntLong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println(myMinIntValue);
+        System.out.println(myMaxIntValue);
+
+    }
+    
+}
